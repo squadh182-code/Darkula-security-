0a641a547a91298e0cf19a0aa9394156c6b61fcb
squadh182-code <squadh182@gmail.com>
2026-09-23 23:03:39 +0600

    Update index.js

--- Security-Bot/index.js
+++ Security-Bot/index.js
@@ -1,1 +1,24 @@
+require("dotenv").config();
 
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildModeration
+  ],
+  partials: [
+    Partials.Message,
+    Partials.Channel,
+    Partials.GuildMember
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Security Bot online as ${client.user.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
