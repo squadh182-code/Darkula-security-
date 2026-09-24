22c3bb7deecfaa4c4ff25b51bd8dd24b6994a25d
squadh182-code <squadh182@gmail.com>
2026-09-25 03:18:29 +0600

    Update userWhitelist.js

--- src/whitelist/userWhitelist.js
+++ src/whitelist/userWhitelist.js
@@ -1,19 +1,47 @@
-const users = new Map();
+const database = require("../database/database");
+
+const data = database.load();
+
+const users = new Map(
+  data.users.map(item => [
+    item.id,
+    item.type
+  ])
+);
+
+function save() {
+  const current = database.load();
+
+  current.users = [...users.entries()]
+    .map(([id, type]) => ({
+      id,
+      type
+    }));
+
+  database.save(current);
+}
 
 function add(userId, type = "All") {
   users.set(userId, type);
+  save();
 }
 
 function remove(userId) {
   users.delete(userId);
+  save();
 }
 
 function has(userId, type = "All") {
-  if (!users.has(userId)) return false;
+  if (!users.has(userId)) {
+    return false;
+  }
 
   const whitelistType = users.get(userId);
 
-  return whitelistType === "All" || whitelistType === type;
+  return (
+    whitelistType === "All" ||
+    whitelistType === type
+  );
 }
 
 function list() {
