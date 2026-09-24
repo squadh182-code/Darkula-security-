47d8624cca226e440010e7bc6792e89dd694fe52
squadh182-code <squadh182@gmail.com>
2026-09-25 03:19:13 +0600

    Update channelWhitelist.js

--- src/whitelist/channelWhitelist.js
+++ src/whitelist/channelWhitelist.js
@@ -1,19 +1,47 @@
-const channels = new Map();
+const database = require("../database/database");
+
+const data = database.load();
+
+const channels = new Map(
+  data.channels.map(item => [
+    item.id,
+    item.type
+  ])
+);
+
+function save() {
+  const current = database.load();
+
+  current.channels = [...channels.entries()]
+    .map(([id, type]) => ({
+      id,
+      type
+    }));
+
+  database.save(current);
+}
 
 function add(channelId, type = "All") {
   channels.set(channelId, type);
+  save();
 }
 
 function remove(channelId) {
   channels.delete(channelId);
+  save();
 }
 
 function has(channelId, type = "All") {
-  if (!channels.has(channelId)) return false;
+  if (!channels.has(channelId)) {
+    return false;
+  }
 
   const whitelistType = channels.get(channelId);
 
-  return whitelistType === "All" || whitelistType === type;
+  return (
+    whitelistType === "All" ||
+    whitelistType === type
+  );
 }
 
 function list() {
