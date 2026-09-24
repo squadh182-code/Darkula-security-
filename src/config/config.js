ea2315a3ee2edf6b6fca16c1b9d412c193450ebd
squadh182-code <squadh182@gmail.com>
2026-09-25 02:24:01 +0600

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  botRoleId: "1401958482264985621",
+
+  // পরে এখানে logs channel ID বসাবো
+  securityLogChannelId: "",
+
+  timeoutDuration: 5 * 60 * 1000,
+
+  spamLimit: 4,
+  mentionLimit: 4,
+  emojiLimit: 4,
+
+  // পরে চাইলে change করতে পারবে
+  maxMessageLength: 2000
+};
