23165dd14c9b04e8d17efa0418053f1aa04d3c44
squadh182-code <squadh182@gmail.com>
2026-09-25 02:25:33 +0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const {
+  Client,
+  GatewayIntentBits,
+  Partials
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildModeration
+  ],
+
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.User
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
