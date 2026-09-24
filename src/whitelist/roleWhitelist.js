b8f88af1a0fa58081b9c47e6b3f1223cbc1bb128
squadh182-code <squadh182@gmail.com>
2026-09-25 03:18:50 +0600

    Update roleWhitelist.js

--- src/whitelist/roleWhitelist.js
+++ src/whitelist/roleWhitelist.js
@@ -1,11 +1,34 @@
-const roles = new Map();
+const database = require("../database/database");
+
+const data = database.load();
+
+const roles = new Map(
+  data.roles.map(item => [
+    item.id,
+    item.type
+  ])
+);
+
+function save() {
+  const current = database.load();
+
+  current.roles = [...roles.entries()]
+    .map(([id, type]) => ({
+      id,
+      type
+    }));
+
+  database.save(current);
+}
 
 function add(roleId, type = "All") {
   roles.set(roleId, type);
+  save();
 }
 
 function remove(roleId) {
   roles.delete(roleId);
+  save();
 }
 
 function has(roleIds, type = "All") {
@@ -13,12 +36,17 @@ function has(roleIds, type = "All") {
     roleIds = [roleIds];
   }
 
-  return roleIds.some((roleId) => {
-    if (!roles.has(roleId)) return false;
+  return roleIds.some(roleId => {
+    if (!roles.has(roleId)) {
+      return false;
+    }
 
     const whitelistType = roles.get(roleId);
 
-    return whitelistType === "All" || whitelistType === type;
+    return (
+      whitelistType === "All" ||
+      whitelistType === type
+    );
   });
 }
 
