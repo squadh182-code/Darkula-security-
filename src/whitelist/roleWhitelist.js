1cbc4c46c1cc7436cf998e2f497c87f6cd66187b
squadh182-code <squadh182@gmail.com>
2026-09-24 23:22:19 +0000

    Update roleWhitelist.js

--- src/whitelist/roleWhitelist.js
+++ src/whitelist/roleWhitelist.js
@@ -1,37 +1,108 @@
-const database = require("../database/database");
+const database =
+  require("../database/database");
 
 const data = database.load();
 
-const roles = new Map(
-  data.roles.map(item => [
-    item.id,
+const roles = new Map();
+
+for (const item of data.roles) {
+  if (
+    item &&
+    item.id &&
     item.type
-  ])
-);
+  ) {
+    if (!roles.has(item.id)) {
+      roles.set(
+        item.id,
+        new Set()
+      );
+    }
+
+    roles.get(item.id).add(item.type);
+  }
+}
 
 function save() {
-  const current = database.load();
+  const current =
+    database.load();
 
-  current.roles = [...roles.entries()]
-    .map(([id, type]) => ({
-      id,
-      type
-    }));
+  current.roles = [];
+
+  for (const [id, types] of roles.entries()) {
+    for (const type of types) {
+      current.roles.push({
+        id,
+        type
+      });
+    }
+  }
 
   database.save(current);
 }
 
-function add(roleId, type = "All") {
-  roles.set(roleId, type);
+function add(
+  roleId,
+  type = "All"
+) {
+  if (!roles.has(roleId)) {
+    roles.set(
+      roleId,
+      new Set()
+    );
+  }
+
+  const types =
+    roles.get(roleId);
+
+  if (type === "All") {
+    types.clear();
+    types.add("All");
+  } else {
+    if (types.has("All")) {
+      return;
+    }
+
+    types.add(type);
+  }
+
   save();
 }
 
-function remove(roleId) {
-  roles.delete(roleId);
-  save();
+function remove(
+  roleId,
+  type = "All"
+) {
+  if (!roles.has(roleId)) {
+    return false;
+  }
+
+  const types =
+    roles.get(roleId);
+
+  if (type === "All") {
+    roles.delete(roleId);
+    save();
+    return true;
+  }
+
+  const removed =
+    types.delete(type);
+
+  if (types.size === 0) {
+    roles.delete(roleId);
+  }
+
+  if (removed) {
+    save();
+  }
+
+  return removed;
 }
 
-function has(roleIds, type = "All") {
+function has(
+  roleIds,
+  type = "All"
+) {
   if (!Array.isArray(roleIds)) {
     roleIds = [roleIds];
   }
@@ -41,22 +112,43 @@ function has(roleIds, type = "All") {
       return false;
     }
 
-    const whitelistType = roles.get(roleId);
+    const types =
+      roles.get(roleId);
 
     return (
-      whitelistType === "All" ||
-      whitelistType === type
+      types.has("All") ||
+      types.has(type)
     );
   });
 }
 
 function list() {
-  return [...roles.entries()];
+  const result = [];
+
+  for (const [id, types] of roles.entries()) {
+    result.push({
+      id,
+      types: [...types]
+    });
+  }
+
+  return result;
+}
+
+function getTypes(roleId) {
+  if (!roles.has(roleId)) {
+    return [];
+  }
+
+  return [
+    ...roles.get(roleId)
+  ];
 }
 
 module.exports = {
   add,
   remove,
   has,
-  list
+  list,
+  getTypes
 };
