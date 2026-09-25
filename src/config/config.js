5e6dd06e0639b0d8620331e5a249091c8a4a176b
squadh182-code <squadh182@gmail.com>
2026-09-25 02:47:56 +0000

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -10,14 +10,19 @@ const config = {
   timeoutDuration:
     5 * 60 * 1000,
 
+  // Spam protection
   spamLimit: 4,
 
+  // Mention protection
   mentionLimit: 3,
 
+  // Emoji protection
   emojiLimit: 3,
 
-  maxMessageLength: 1500,
+  // Long message protection
+  maxMessageLength: 500,
 
+  // Similar message detection
   similarityThreshold: 0.85
 };
 
