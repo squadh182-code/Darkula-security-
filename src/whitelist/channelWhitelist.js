ee0c7105eb210f2bfa0295446c1b9a81ff7c6157
squadh182-code <squadh182@gmail.com>
2026-09-25 01:28:50 +0000

    Update channelWhitelist.js

--- src/whitelist/channelWhitelist.js
+++ src/whitelist/channelWhitelist.js
@@ -1,144 +1,64 @@
 const database =
   require("../database/database");
 
-const data = database.load();
-
-const channels = new Map();
-
-for (const item of data.channels) {
-  if (
-    item &&
-    item.id &&
-    item.type
-  ) {
-    if (!channels.has(item.id)) {
-      channels.set(
-        item.id,
-        new Set()
-      );
-    }
-
-    channels
-      .get(item.id)
-      .add(item.type);
-  }
-}
-
-function save() {
-  const current =
-    database.load();
-
-  current.channels = [];
-
-  for (const [id, types] of channels.entries()) {
-    for (const type of types) {
-      current.channels.push({
-        id,
-        type
-      });
-    }
-  }
-
-  database.save(current);
-}
-
-function add(
+async function add(
   channelId,
   type = "All"
 ) {
-  if (!channels.has(channelId)) {
-    channels.set(
+  if (type === "All") {
+    await database.removeAllWhitelist(
       channelId,
-      new Set()
+      "channel"
     );
   }
 
-  const types =
-    channels.get(channelId);
-
-  if (type === "All") {
-    types.clear();
-    types.add("All");
-  } else {
-    if (types.has("All")) {
-      return;
-    }
-
-    types.add(type);
-  }
-
-  save();
-}
-
-function remove(
-  channelId,
-  type = "All"
-) {
-  if (!channels.has(channelId)) {
-    return false;
-  }
-
-  const types =
-    channels.get(channelId);
-
-  if (type === "All") {
-    channels.delete(channelId);
-    save();
-    return true;
-  }
-
-  const removed =
-    types.delete(type);
-
-  if (types.size === 0) {
-    channels.delete(channelId);
-  }
-
-  if (removed) {
-    save();
-  }
-
-  return removed;
-}
-
-function has(
-  channelId,
-  type = "All"
-) {
-  if (!channels.has(channelId)) {
-    return false;
-  }
-
-  const types =
-    channels.get(channelId);
-
-  return (
-    types.has("All") ||
-    types.has(type)
+  await database.addWhitelist(
+    channelId,
+    "channel",
+    type
   );
 }
 
-function list() {
-  const result = [];
-
-  for (const [id, types] of channels.entries()) {
-    result.push({
-      id,
-      types: [...types]
-    });
+async function remove(
+  channelId,
+  type = "All"
+) {
+  if (type === "All") {
+    return database.removeAllWhitelist(
+      channelId,
+      "channel"
+    );
   }
 
-  return result;
+  return database.removeWhitelist(
+    channelId,
+    "channel",
+    type
+  );
 }
 
-function getTypes(channelId) {
-  if (!channels.has(channelId)) {
-    return [];
-  }
+async function has(
+  channelId,
+  type = "All"
+) {
+  return database.hasWhitelist(
+    channelId,
+    "channel",
+    type
+  );
+}
 
-  return [
-    ...channels.get(channelId)
-  ];
+async function list() {
+  return database.listWhitelists(
+    "channel"
+  );
+}
+
+async function getTypes(channelId) {
+  return database.getWhitelists(
+    channelId,
+    "channel"
+  );
 }
 
 module.exports = {
