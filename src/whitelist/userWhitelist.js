4c123331c63c86faf781234d45b22a095c6487b4
squadh182-code <squadh182@gmail.com>
2026-09-25 01:27:51 +0000

    Update userWhitelist.js

--- src/whitelist/userWhitelist.js
+++ src/whitelist/userWhitelist.js
@@ -1,141 +1,64 @@
 const database =
   require("../database/database");
 
-const data = database.load();
-
-const users = new Map();
-
-for (const item of data.users) {
-  if (
-    item &&
-    item.id &&
-    item.type
-  ) {
-    if (!users.has(item.id)) {
-      users.set(item.id, new Set());
-    }
-
-    users.get(item.id).add(item.type);
-  }
-}
-
-function save() {
-  const current =
-    database.load();
-
-  current.users = [];
-
-  for (const [id, types] of users.entries()) {
-    for (const type of types) {
-      current.users.push({
-        id,
-        type
-      });
-    }
-  }
-
-  database.save(current);
-}
-
-function add(
+async function add(
   userId,
   type = "All"
 ) {
-  if (!users.has(userId)) {
-    users.set(
+  if (type === "All") {
+    await database.removeAllWhitelist(
       userId,
-      new Set()
+      "user"
     );
   }
 
-  const types =
-    users.get(userId);
-
-  // All replaces specific types
-  if (type === "All") {
-    types.clear();
-    types.add("All");
-  } else {
-    // If All exists, specific type is unnecessary
-    if (types.has("All")) {
-      return;
-    }
-
-    types.add(type);
-  }
-
-  save();
-}
-
-function remove(
-  userId,
-  type = "All"
-) {
-  if (!users.has(userId)) {
-    return false;
-  }
-
-  const types =
-    users.get(userId);
-
-  if (type === "All") {
-    users.delete(userId);
-    save();
-    return true;
-  }
-
-  const removed =
-    types.delete(type);
-
-  if (types.size === 0) {
-    users.delete(userId);
-  }
-
-  if (removed) {
-    save();
-  }
-
-  return removed;
-}
-
-function has(
-  userId,
-  type = "All"
-) {
-  if (!users.has(userId)) {
-    return false;
-  }
-
-  const types =
-    users.get(userId);
-
-  return (
-    types.has("All") ||
-    types.has(type)
+  await database.addWhitelist(
+    userId,
+    "user",
+    type
   );
 }
 
-function list() {
-  const result = [];
-
-  for (const [id, types] of users.entries()) {
-    result.push({
-      id,
-      types: [...types]
-    });
+async function remove(
+  userId,
+  type = "All"
+) {
+  if (type === "All") {
+    return database.removeAllWhitelist(
+      userId,
+      "user"
+    );
   }
 
-  return result;
+  return database.removeWhitelist(
+    userId,
+    "user",
+    type
+  );
 }
 
-function getTypes(userId) {
-  if (!users.has(userId)) {
-    return [];
-  }
+async function has(
+  userId,
+  type = "All"
+) {
+  return database.hasWhitelist(
+    userId,
+    "user",
+    type
+  );
+}
 
-  return [
-    ...users.get(userId)
-  ];
+async function list() {
+  return database.listWhitelists(
+    "user"
+  );
+}
+
+async function getTypes(userId) {
+  return database.getWhitelists(
+    userId,
+    "user"
+  );
 }
 
 module.exports = {
