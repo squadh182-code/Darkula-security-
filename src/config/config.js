b330bccca4f5c660e0ebf8a3c064c74b18adce15
squadh182-code <squadh182@gmail.com>
2026-09-25 02:41:39 +0000

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,24 @@
-module.exports = {
+const config = {
   botRoleId: "1401958482264985621",
 
-  securityLogChannelId: "1552785563369742437",
+  securityLogChannelId:
+    "1552785563369742437",
 
-  guildId: "1397974278934106182",
+  guildId:
+    "1397974278934106182",
 
-  timeoutDuration: 5 * 60 * 1000,
+  timeoutDuration:
+    5 * 60 * 1000,
 
   spamLimit: 4,
+
   mentionLimit: 3,
+
   emojiLimit: 3,
 
-  maxMessageLength: 2000,
+  maxMessageLength: 1500,
 
   similarityThreshold: 0.85
 };
+
+module.exports = config;
